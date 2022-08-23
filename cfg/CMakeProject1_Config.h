c6fdb17be4892fd38b3622361f0b51da20e7de87
Nathan Hood <nathanjhood@googlemail.com>
2022-08-23 07:12:17 +0200

    Added project config header
    To be included in "main.cpp" to provide project-wide confuration information to be used within the project itself.
    
    This file is auto-generated by the project configuation (it is dafe to delete it entirely before running the configuration step). To add or edit the contents of this file, please specify in the associated "...Config.h.in" file, and re-run the project configuration step.

---
+++ cfg/CMakeProject1_Config.h
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ *
+ * @file Config.h
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#define companyName "yourCompany"
+#define projectName "CMakeProject1"
+#define projectDescription "Generic CMake Project with in-built package manager"
+#define projectVersion "v0.0.0.1"
+#define projectWebsite "https://github.com/StoneyDSP/CMakeProject1"
+
+#endif // CONFIG_H_INCLUDED
