e8c4523765a2bd1922480243e214f63d2b5dce31
Nathan Hood <nathanjhood@googlemail.com>
2022-08-23 10:58:25 +0200

    Enhanced "docs" file

---
+++ docs/navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[0,0,0],
+"files.html":[0,0],
+"globals.html":[0,1,0],
+"globals_func.html":[0,1,1],
+"index.html":[],
+"main_8cpp.html":[0,0,0,0],
+"main_8cpp.html#a91359998962e0bc9e191e0220c63c7cf":[0,0,0,0,0],
+"main_8h.html":[0,0,0,1],
+"main_8h.html#ae66f6b31b5ad750f1fe042a706a4e3d4":[0,0,0,1,0],
+"pages.html":[]
+};
