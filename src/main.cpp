76a1ea4b6b3b164cffa79f08b432950348fbf977
Nathan Hood <nathanjhood@googlemail.com>
2022-08-23 07:14:54 +0200

    Implemented initial main.cpp
    This implementation utilizes the packages "fmt" and "cppunit" for it's operations. These are marked as "dependencies" in vpkg.json, and imported to the CMake project in the top-level CMakeLists.txt file. Once successfully imported, you will be able to use these third-party packes in your project, including associated Intellisense!

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+﻿
+
+// CMakeProject1.cpp : Defines the entry point for the application.
+
+#include "main.h"
+
+#ifndef CPPUNIT_ASSERTER_H
+#else CPPUNIT_ASSERTER_H
+#endif CPPUNIT_ASSERTER_H
+
+int main()
+{
+	fmt::print("Hello, from {}!", projectName); std::cout << std::endl;
+	fmt::print("Version {}!", projectVersion); std::cout << std::endl;
+	fmt::print("Description: {}!", projectDescription); std::cout << std::endl;
+	fmt::print("Bought to you by {}!", companyName); std::cout << std::endl;
+	fmt::print("Find out more at {}!", projectWebsite); std::cout << std::endl;
+	
+	std::cout << std::endl;
+
+	return 0;
+}
