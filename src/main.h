17b4393b3cfe1e10820bc596019456bde90604d2
Nathan Hood <nathanjhood@googlemail.com>
2022-08-23 07:05:29 +0200

    Created main.h for project-wide includes / defines

---
+++ src/main.h
@@ -0,0 +1,31 @@
+﻿/*******************************************************************************
+ *
+ * @file main.h
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+// Global app config header.
+#include "../cfg/CMakeProject1_Config.h"
+
+// Standard C++ Library includes go here.
+#include <assert.h>
+#include <iostream>
+#include <string.h>
+
+// TODO: Reference additional headers your program requires here.
+#include <fmt/format.h>
+#include <cppunit/Asserter.h>
+
+#endif // MAIN_H_INCLUDED
