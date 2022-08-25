65f4ee7535e23579b0c3b0d5977f0ea681a4ee5d
Nathan Hood <nathanjhood@googlemail.com>
2022-08-26 01:19:50 +0200

    Monitoring cl compiler architecture...

--- cfg/CMakeProject1_Config.h
+++ cfg/CMakeProject1_Config.h
@@ -24,5 +24,6 @@
 #define projectDescription "Generic CMake Project with in-built package manager"
 #define projectVersion "v0.0.0.1"
 #define projectWebsite "https://stoneydsp.github.io/CMakeProject1/html/index.html"
+#define cppComiler "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.33.31629/bin/Hostx86/x86/cl.exe"
 
 #endif // CONFIG_H_INCLUDED
