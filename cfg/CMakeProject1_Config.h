d30f650352f78184187649d15d82f7fbdf0974d9
Nathan Hood <nathanjhood@googlemail.com>
2022-09-06 17:34:24 +0200

    Fixed local-version project

--- cfg/CMakeProject1_Config.h
+++ cfg/CMakeProject1_Config.h
@@ -20,9 +20,9 @@
  *
  */
 #define companyName "StoneyDSP"
-#define projectName "StoneyDSP"
-#define projectDescription ""
+#define projectName "CMakeProject1"
+#define projectDescription "Generic CMake Project with in-built package manager"
 #define projectVersion "v0.0.0.1"
-#define projectWebsite "https://stoneydsp.github.io/html/index.html"
+#define projectWebsite "https://stoneydsp.github.io/CMakeProject1/html/index.html"
 
 #endif // CONFIG_H_INCLUDED
