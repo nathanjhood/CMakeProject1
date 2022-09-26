2de98b53397e6c0885b9a3743d47f94e16acdfdd
Nathan Hood <nathanjhood@googlemail.com>
2022-09-26 18:30:43 +0200

    Enhanced Powershell scripts

--- support/powershell/Microsoft.PowerShell_profile.ps1
+++ support/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 Set-Location E:\CMakeProject1\
-Import-Module 'E:\CMakeProject1\extern\vcpkg\scripts\posh-vcpkg'
+."C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 10.0.22621.0
+Import-Module E:\CMakeProject1\extern\vcpkg\scripts\posh-vcpkg
+.\extern\vcpkg\vcpkg integrate install
