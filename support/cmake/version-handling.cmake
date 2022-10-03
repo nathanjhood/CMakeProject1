46ae5ee7f92822900fdd91b9f019ce59e36a77dd
Nathan Hood <nathanjhood@googlemail.com>
2022-10-03 02:42:19 +0200

    Working on MacOS version file...

--- support/cmake/version-handling.cmake
+++ support/cmake/version-handling.cmake
@@ -2,7 +2,7 @@
 cmake_minimum_required(VERSION 3.7...3.24.2)
 
 # set version using external "VERSION" file
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION LOCAL_VERSION)
+file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION.txt LOCAL_VERSION)
 string(REGEX MATCH "([0-999]*).[0-999].[0-999].[0-999]" _ ${LOCAL_VERSION})
 set(LOCAL_VERSION_MAJOR ${CMAKE_MATCH_1})
 string(REGEX MATCH "[0-999].([0-999]*).[0-999].[0-999]" _ ${LOCAL_VERSION})
