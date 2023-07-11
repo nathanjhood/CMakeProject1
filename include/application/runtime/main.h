9b5fd74c10803dea9fdcfb86a330f4af24b66f34
StoneyDSP <nathanjhood@googlemail.com>
2023-07-11 09:21:12 +0200

    Enhanced runtime features

--- include/application/runtime/main.h
+++ include/application/runtime/main.h
@@ -1,12 +1,11 @@
-#ifndef CLI_RUNTIME_MAIN_H_INCLUDED
-#define CLI_RUNTIME_MAIN_H_INCLUDED
+#ifndef APPLICATION_RUNTIME_MAIN_H_INCLUDED
+#define APPLICATION_RUNTIME_MAIN_H_INCLUDED
 
-#include "../../application/sharedlib.h"
+//#include "include/application/sharedlib.h"
 
 #include "../../application/configuration.h"
-
 
 /** Main entry point for all C++ runtime applications. */
 int main();
 
-#endif // CLI_RUNTIME_MAIN_H_INCLUDED
+#endif // APPLICATION_RUNTIME_MAIN_H_INCLUDED
--- src/application/runtime/main.cpp
+++ src/application/runtime/main.cpp
@@ -1,8 +1,8 @@
-#include "../../../include/application/runtime/main.h"
+#include "../../include/application/runtime/main.h"
 
 int main()
 {
-    application::Configuration Configuration;
+    // application::Configuration Configuration;
 
 #ifndef APPLICATION_CONFIGURATION_H_INCLUDED
 
@@ -11,9 +11,35 @@ int main()
 
 #else
 
-    std::cout << MSYSTEM << "\n" << std::endl;
-    std::cout << MSYSTEM_CARCH << "\n" << std::endl;
-    std::cout << MSYSTEM_CHOST << "\n" << std::endl;
+    #ifdef application_VERSION_TWEAK
+
+    #  define application_VERSION_INFO_CHECKED 1
+
+    char const* versionString = "Version" ":" " " application_VERSION;
+    char const* sourceString = "Source" ":" " " application_REPOSITORY;
+
+    std::cout << "\n" << std::endl;
+    std::cout << "StoneyDSP" << std::endl;
+    std::cout << "CMakeProject1" << std::endl;
+    std::cout << "\n" << std::endl;
+    std::cout << "A simple CMake project for testing build configs, toolchains, and more." << std::endl;
+    std::cout << "\n" << std::endl;
+    std::cout << "MIT License" << std::endl;
+    std::cout << "Copyright (c) 2022 - 2023, StoneyDSP" << std::endl;
+    std::cout << "All rights reserved." << std::endl;
+    std::cout << "\n" << std::endl;
+    std::cout << versionString << std::endl;
+    std::cout << sourceString << std::endl;
+    std::cout << "\n" << std::endl;
+
+    #else
+
+    #  define application_VERSION_INFO_CHECKED 0
+
+    std::cout << "Version header not found!" << std::endl;
+    std::cout << "Nothing to report." << "\n" << std::endl;
+
+    #endif
 
 #endif // APPLICATION_CONFIGURATION_H_INCLUDED
 
