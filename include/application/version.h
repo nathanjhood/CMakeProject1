99f55edcc2103a1a28069c30e9c0a1e11a7915db
StoneyDSP <nathanjhood@googlemail.com>
2023-07-11 09:25:31 +0200

    Improved 'version.h' automation

--- include/application/version.h
+++ include/application/version.h
@@ -1,4 +1,4 @@
-// application v1.0.1.daaf6fb255dbb66888b3e9a7383a242b0c71adc0
+// application v1.0.1.63cd5907aa44a1b5ff60ba2a75e7f27b88d1737e
 // Version control info
 
 #ifndef application_VERSION_H_INCLUDED
@@ -7,8 +7,8 @@
 #define application_VERSION_MAJOR "1"
 #define application_VERSION_MINOR "0"
 #define application_VERSION_PATCH "1"
-#define application_VERSION_TWEAK "daaf6fb255dbb66888b3e9a7383a242b0c71adc0"
-#define application_VERSION "1.0.1.daaf6fb255dbb66888b3e9a7383a242b0c71adc0"
+#define application_VERSION_TWEAK "63cd5907aa44a1b5ff60ba2a75e7f27b88d1737e"
+#define application_VERSION "1.0.1.63cd5907aa44a1b5ff60ba2a75e7f27b88d1737e"
 #define application_REPOSITORY "https://github.com/StoneyDSP/CMakeProject1.git"
 
 #endif // application_VERSION_H_INCLUDED
