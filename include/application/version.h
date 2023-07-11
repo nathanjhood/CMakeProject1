affeb568b44765fbb4d5731179f45f809135c05a
StoneyDSP <nathanjhood@googlemail.com>
2023-07-11 09:24:22 +0200

    Generated 'version.h' from source control :)

---
+++ include/application/version.h
@@ -0,0 +1,15 @@
+// application v1.0.1.daaf6fb255dbb66888b3e9a7383a242b0c71adc0
+// Version control info
+
+#ifndef application_VERSION_H_INCLUDED
+#define application_VERSION_H_INCLUDED
+
+#define application_VERSION_MAJOR "1"
+#define application_VERSION_MINOR "0"
+#define application_VERSION_PATCH "1"
+#define application_VERSION_TWEAK "daaf6fb255dbb66888b3e9a7383a242b0c71adc0"
+#define application_VERSION "1.0.1.daaf6fb255dbb66888b3e9a7383a242b0c71adc0"
+#define application_REPOSITORY "https://github.com/StoneyDSP/CMakeProject1.git"
+
+#endif // application_VERSION_H_INCLUDED
+
